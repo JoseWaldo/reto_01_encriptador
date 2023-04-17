ac3e098b89769f0a93d9fa43792b0629c8a9c888
Jose Waldo <josferw@gmail.com>
2023-04-16 20:06:54 -0500

    feat: add the structure html and init the logic of the program

---
+++ main.js
@@ -0,0 +1,59 @@
+/*
+Reglas de Encriptación:
+La letra "e" es convertida para "enter"
+La letra "i" es convertida para "imes"
+La letra "a" es convertida para "ai"
+La letra "o" es convertida para "ober"
+La letra "u" es convertida para "ufat"
+aeiou
+aienterimes
+*/
+
+const $d = document;
+
+const inputText = $d.querySelector("#inputText");
+const btnEncriptar = $d.querySelector("#btnEncriptar");
+const btnDesencriptar = $d.querySelector("#btnDesencriptar");
+
+const REGLAS_DE_ENCRIPTACION = {
+  e: "enter",
+  i: "imes",
+  a: "ai",
+  o: "ober",
+  u: "ufat",
+};
+
+const encriptar = (e) => {
+  e.preventDefault();
+  let text = inputText.value;
+  let longitud = text.length;
+  let i = 0;
+  while (i < longitud) {
+    console.log(i);
+    const char = text[i];
+    if ("aeiou".indexOf(char) === -1) {
+      continue;
+    }
+    console.log(char);
+    const palabraReemplazo = REGLAS_DE_ENCRIPTACION[char];
+    text = text.replace(char, palabraReemplazo);
+    i += palabraReemplazo.length;
+    longitud = text.length;
+  }
+
+  //   for (let i = 0; i < longitud; i++) {
+  //     console.log(i);
+  //     const char = text[i];
+  //     if ("aeiou".indexOf(char) === -1) {
+  //       continue;
+  //     }
+  //     console.log(char);
+  //     const palabraReemplazo = REGLAS_DE_ENCRIPTACION[char];
+  //     text = text.replace(char, palabraReemplazo);
+  //     i += palabraReemplazo.length;
+  //     longitud = text.length;
+  //   }
+  console.log(text);
+};
+
+btnEncriptar.addEventListener("click", (e) => encriptar(e));
